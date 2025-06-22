ab23217f958de7f8a03fc2931e97c842329825b3
Yawner01 <wesallen111@gmail.com>
2025-06-22 13:19:21 -0400

    Added Products controller and added products class

---
+++ RetailClothingStockManager/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailClothingStockManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        //database connection logic here
+
+        [HttpPost]
+        public IActionResult AddProduct([FromBody] Product Newproduct)
+        {
+            //logic to add the product to the database
+            //return appropriate response
+            return Ok();
+        }
+    }
+}
---
+++ RetailClothingStockManager/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace RetailClothingStockManager
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+    }
+}
